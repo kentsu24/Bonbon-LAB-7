ef76956f0bc2a711e4f46aeecc2f334933c126bb
kentsu24 <kentbonbon24@gmail.com>
2024-10-17 19:33:00 +0800

    Update LAB 7 BONBON.py

--- LAB 7 BONBON.py
+++ LAB 7 BONBON.py
@@ -1,36 +1,27 @@
 prelim_grade = float(input("Enter Prelimanary Period Grade:"))
 midterm_grade = float(input("Enter Midterm Period Grade:"))
-final_grade = float (input("Enter Final Period Grade:"))
-        
-if ( prelim_grade >=40 and prelim_grade <= 100 and midterm_grade >=40 and midterm_grade <= 100 and  final_grade >=40 and final_grade<= 100):
-        print ("Error: Grade input must between the range of 40-100.")
-            
-grade_points = round((prelim_grade * 0.3333) + (midterm_grade * 0.3333) + (final_grade * 0.3333))
-print("nFinal Grade Points:",grade_points)
-print("Grade Desription:" + str (grade_points))
+final_grade = float(input("Enter Final Period Grade:"))
 
-if grade_points >= 99 and grade_points <= 100:
-        print ("Excellent")
-elif grade_points ==1.25:
-        print ("Outstanding")
-elif grade_points == 1.50:
-        print ("Superior")
-elif grade_points == 1.75:
-        print ("Very Good")
-elif grade_points == 2.00:
-        print ("Good")
-elif grade_points == 2.25:
-        print ("Satisfactory")
-elif grade_points == 2.50:
-        print ("Fairly Satisfactory")
-elif grade_points == 2.75:
-        print ("Fair")
-elif grade_points == 3.00:
-        print ("Passed")
+if (prelim_grade < 40 or prelim_grade > 100 or midterm_grade < 40 or midterm_grade > 100 or final_grade < 40 or final_grade > 100):
+    print("Error: Grade input must be between the range of 40-100.")
 else:
-        print ("Failed")
-        
-grade_points >= 100 and <= 98
+    grade_points = round((prelim_grade * 0.3333) + (midterm_grade * 0.3333) + (final_grade * 0.3333))
+    print("Final Grade Points:", grade_points)
+    print("Grade Description:" + str(grade_points))
+
+    if grade_points >= 90 and grade_points <= 100:
+        print("Excellent")
+    elif grade_points >= 80 and grade_points <= 89:
+        print("Very Good")
+    elif grade_points >= 70 and grade_points <= 79:
+        print("Good")
+    elif grade_points >= 60 and grade_points <= 69:
+        print("Satisfactory")
+    elif grade_points >= 50 and grade_points <= 59:
+        print("Fair")
+    else:
+        print("Failed")
+
         
 
 
